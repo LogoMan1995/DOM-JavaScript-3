9911b348770077b072cafd6883acd2d078820ca5
David <david.grigorian777@gmail.com>
2024-09-28 21:47:12 +0400

    audio-player-part-3

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let jukebox = document.querySelector("ul.player");
-
+let jukebox = document.querySelector('ul.player');
 jukebox.addEventListener('click', (e) => {
-  let audioPlayer = document.createElement("audio");
-  let songName = e.target.getAttribute("data-src");
-  audioPlayer.id = "player";
-  e.target.id = "playing";
-  audioPlayer.src = songName;
-  document.body.appendChild(audioPlayer)
-  audioPlayer.play()
+  let songName = e.target.getAttribute('data-src');
+  let songPlaying = document.querySelector('#player');
 
+  if (songPlaying) {
+    if (songPlaying.paused) {
+      songPlaying.play();
+      e.target.id = 'playing';
+    } else {
+      songPlaying.pause();
+      e.target.id = 'paused';
+    }
+  } else {
+    let audioPlayer = document.createElement('audio');
+    audioPlayer.id = 'player';
+    e.target.id = 'playing';
+    audioPlayer.src = songName;
+    document.body.appendChild(audioPlayer);
+    audioPlayer.play();
 
-  audioPlayer.addEventListener('ended', () => {
-    audioPlayer.parentNode.removeChild(audioPlayer);
-    e.target.id = ''
-  })
-})
-
-
-
-
+    audioPlayer.addEventListener('ended', () => {
+      e.target.id = '';
+      audioPlayer.parentNode.removeChild(audioPlayer);
+    });
+  }
+});
